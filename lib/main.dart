d429df39e307f29c20e6f36e9e6940fca313c2b1
mohitga <mohitgangwar09@gmail.com>
2022-04-14 23:22:10 +0530

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(fontFamily: "Roboto"),
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'Sizer',
       home: LoginPage(),
     );
